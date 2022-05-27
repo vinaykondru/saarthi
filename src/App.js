4a66b2d111e2c9f61e6fa8f314c75955cccb6fd2
vinay <vinaykumarkondru@gmail.com>
2022-05-27 10:58:11 +0530

    lastcommit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import {BrowserRouter, Routes,Route} from 'react-router-dom';
 import Albums from './pages/Albums'
 import Users from './pages/Users';
 import Photos from './pages/Photos';
+import Menu from "../src/data.js";
+import Sample from './Sample';
 function App() {
   
   return (
     <>
+    <Sample data={Menu} />
      <BrowserRouter>
      <Routes>
        <Route path='/' element={<Users />} />
